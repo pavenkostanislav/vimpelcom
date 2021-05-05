67cfc022f7b9c176c27fa22b442df3a32bd761a0
Stanislav Pavenko (MacBook Pro) <pavenko@i-teco.ru>
2021-05-05 15:31:23 +0300

    price filter

--- src/app/number-space.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NumberSpacePipe } from './number-space.pipe';
-
-describe('NumberSpacePipe', () => {
-  it('create an instance', () => {
-    const pipe = new NumberSpacePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
